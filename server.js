104389bd9c1feca1d6a25b26fdaa974741969ba5
webclidev <webclidev@gmail.com>
2024-12-10 18:08:14 +0530

    feat: serve HTML file for root endpoint

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const PORT = 8000;
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Om Namah Shivay");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
