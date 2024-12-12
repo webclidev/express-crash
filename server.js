7e24e698612e6c64c1dc4218841bf15dc7f49517
webclidev <webclidev@gmail.com>
2024-12-12 09:43:51 +0530

    feat: add status codes and error handling for posts API

--- server.js
+++ server.js
@@ -16,15 +16,23 @@ app.get("/api/posts", (req, res) => {
   const limit = parseInt(req.query.limit);
 
   if (!isNaN(limit) && limit > 0) {
-    res.json(posts.slice(0, limit));
+    res.status(200).json(posts.slice(0, limit));
   } else {
-    res.json(posts);
+    res.status(200).json(posts);
   }
 });
 
 app.get("/api/posts/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  res.json(posts.filter((post) => post.id === id));
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    res
+      .status(404)
+      .json({ message: `A post with the id of ${id} was not found` });
+  } else {
+    res.status(200).json(post);
+  }
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
