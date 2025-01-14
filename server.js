a56c76fcb995eeef18bd6fa15f2f6d4cda9d3523
webclidev <webclidev@gmail.com>
2025-01-14 09:43:31 +0530

    feat: add logger middleware to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import posts from "./routes/posts.js";
+import logger from "./middleware/logger.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,6 +13,9 @@ const app = express();
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Logger middleware
+app.use(logger);
 
 // setup static folder
 app.use(express.static(path.join(__dirname, "public")));
