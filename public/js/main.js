37905cc1bb7455da49d302102818cd67541cfc6a
webclidev <webclidev@gmail.com>
2025-03-01 19:47:23 +0530

    feat: add form to create new posts and improve styling in index.html

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 const outputDiv = document.getElementById("output");
 const getPostsBtn = document.getElementById("get-posts");
+const addPostForm = document.getElementById("add-post-form");
 
 // Get and show posts
 async function showPosts() {
@@ -22,5 +23,31 @@ async function showPosts() {
   }
 }
 
+// Add post
+async function addPost(e) {
+  e.preventDefault();
+  const formData = new FormData(this);
+  const title = formData.get("title");
+
+  try {
+    const res = await fetch("/api/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title }),
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to add post");
+    }
+
+    showPosts();
+  } catch (error) {
+    console.log("Error adding post");
+  }
+}
+
 // Event listeners
 getPostsBtn.addEventListener("click", showPosts);
+addPostForm.addEventListener("submit", addPost);
